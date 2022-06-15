12f5e300d572a6b4d3f407e5af3c01412fa0747c
bog-danius <jenesius1998@gmail.com>
2022-06-15 19:01:15 +0300

    ...

--- file.js
+++ file.js
@@ -11,3 +11,28 @@ setInterval(function () {
   });
   $(".text").toggleClass("toggled");
 }, 5000);
+
+jQuery(document).ready(function () {
+  jQuery(".send-form").click(function () {
+    var form = jQuery(this).closest("form");
+    if (form.valid()) {
+      //form.css("opacity ", ".5");
+
+      var actUrl = form.attr("action");
+      jQuery.ajax({
+        url: actUrl,
+        type: "post",
+        dataType: "html",
+        data: form.serialize(),
+        success: function (data) {
+          //form.html(data);
+          //form.css("opacity", "1");
+          form.find(".status").html("форма отправлена успешно");
+        },
+        error: function () {
+          form.find(".status").html("серврная ошибка");
+        },
+      });
+    }
+  });
+});
---
+++ sender.php
@@ -0,0 +1,14 @@
+<?php
+$to = "jenesius1998@gmail.com"
+$name = $_POST['name'];
+$text = $_POST['text'];
+$data = data("d.m,Y");
+$time = data ("h:i");
+$from = $email;
+$subject = "Заявка с сайта";
+$msg ="
+Имя : $name /n
+Почта :$email /n
+mail($to , $subject , $msg , "From : $to");
+?>
+
